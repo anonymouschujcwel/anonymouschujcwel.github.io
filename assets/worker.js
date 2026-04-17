fe50266ffd76f91f0d03bad9cc4ee406a1be4fee
SzybkiInwalida <szybki.inwalida@gmail.com>
2026-04-17 20:52:16 +0000

    git push

--- assets/worker.js
+++ assets/worker.js
@@ -1,3 +1,7 @@
+self.addEventListener('install', () => {
+    self.skipWaiting();
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
